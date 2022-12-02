065a811f55321cc0f9aeead3365df8090d1b9662
Raziphel <raziphel@gmail.com>
2022-12-01 19:44:56 -0600

    fix

--- cogs/constants/message_rewards.py
+++ cogs/constants/message_rewards.py
@@ -86,7 +86,7 @@ class Message_Rewards(Cog):
                 await message.clear_reactions()
                 coin = choice([100, 200, 300])
                 c.coin += coin
-                msg = await channel.send(embed=utils.SpecialEmbed(desc=f"{user} found **{coin} {coin_e}x**", footer=f"", guild=guild))
+                msg = await channel.send(embed=utils.SpecialEmbed(desc=f"{user} found **{coin} {coin_e}x**", footer=f""))
                 #try: await log.send(embed=utils.LogEmbed(type="special", title=f"{user} found a reward!", desc=f"{user} found **{coin} {coin_e}x**", guild=guild))
                 #except: pass
 
