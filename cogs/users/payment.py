4514c26d21c7033889fce32bf1c8414624e9d617
Raziphel <raziphel@gmail.com>
2022-12-10 20:11:52 -0600

    Payment fixes

--- cogs/users/payment.py
+++ cogs/users/payment.py
@@ -1,115 +1,42 @@
 # Discord
 from discord.ext.commands import command, cooldown, BucketType, Cog
-from discord import Message, User
+from discord import Member, Message, User
 # Utils
 import utils
-
-
-class TooPoor(BaseException):
-    pass
-
 
 class Payment(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @property  #! The members logs
+    def server_log(self):
+        return self.bot.get_channel(self.bot.config['channels']['server']) #?Coins log channel
 
-
-    @cooldown(1, 86400, BucketType.channel) #! So thats a day in seconds
+    @cooldown(1, 360, BucketType.channel)
     @command(aliases=['send'])
-    async def pay(self, ctx, receiver:User=None, gems:int=0):
-        '''
-        sending payemnts to other members
-        '''
-        #? Check if bot is connected!
-        if self.bot.connected == False:
-            return
-
-
+    async def pay(self, ctx, receiver:User=None, amount:int=0):
+        '''sending payemnts to other members'''
         if not receiver:
-            await ctx.send(embed=utils.SpecialEmbed(description=f"{ctx.author.mention} `.pay (user) (amount)`", guild=ctx.guild))
+            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} `->pay (user) (amount)`"))
             return
-
-        c = utils.Currency.get(ctx.author.id)
-        c_r = utils.Currency.get(receiver.id)
-
-        #* Emojis
-        amethyst_e = "<:Amethyst:766123218732843019>"
-        diamond_e = "<:Diamond:766123219609976832>"
-        emerald_e = "<:Emerald:766123219731611668>"
-        phel_e = "<:PhelStone:766123219781419020>"
-        silver_e = "<:Silver:766123219761233961>"
-        gold_e = "<:GoldIngot:766123219827949596>"
-        sapphire_e = "<:Sapphire:766123220201635850>"
-        ruby_e = "<:Ruby:766123219928481852>"
-
         if receiver == ctx.author:
-            await ctx.send(embed=utils.SpecialEmbed(description=f"{ctx.author.mention} You can't pay yourself gems! stupid...", guild=ctx.guild))
+            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} You can't pay yourself coins! stupid..."))
             return
-        if gems <= 0 or gems > 100:
-            await ctx.send(embed=utils.SpecialEmbed(description=f"{ctx.author.mention} Has to be more than 0 and less than 100 gems/ingots!~", guild=ctx.guild))
+        if amount <= 10:
+            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} Has to be more than 10!"))
             return
-
-        embed = utils.SpecialEmbed(author=f"Click the emote for currency type! (Thanks For Donating!)", desc=f"{silver_e} Silver\n{gold_e} Gold\n{emerald_e} Emerald\n{diamond_e} Diamond", guild=ctx.guild)
-        msg = await ctx.send(embed=embed)
-
-        # adds the reactions
-        await msg.add_reaction(silver_e)
-        await msg.add_reaction(gold_e)
-        await msg.add_reaction(emerald_e)
-        await msg.add_reaction(diamond_e)
-
-        try:
-
-            # Watches for the reactions
-            check = lambda x, y: y.id == ctx.author.id and x.message.id == msg.id and str(x.emoji) in [silver_e, gold_e, emerald_e, diamond_e]
-            r, _ = await self.bot.wait_for('reaction_add', check=check)
-            if str(r.emoji) == silver_e:
-                await utils.GemFunction.pay_exchange(user=ctx.author)
-                if c.silver < gems:
-                    raise TooPoor
-                c_r.silver += gems
-                c.silver -= gems
-
-            if str(r.emoji) == gold_e:
-                await utils.GemFunction.pay_exchange(user=ctx.author)
-                if c.gold < gems:
-                    raise TooPoor
-                c_r.gold += gems
-                c.gold -= gems
-
-            if str(r.emoji) == emerald_e:
-                await utils.GemFunction.pay_exchange(user=ctx.author)
-                if c.emerald < gems:
-                    raise TooPoor
-                c_r.emerald += gems
-                c.emerald -= gems
-
-            if str(r.emoji) == diamond_e:
-                await utils.GemFunction.pay_exchange(user=ctx.author)
-                if c.diamond < gems:
-                    raise TooPoor
-                c_r.diamond += gems
-                c.diamond -= gems
-
-
-        except TooPoor:
-            await ctx.send(f'{ctx.author.mention} Payment denied. You would go in debt stupid!!!')
+        c = utils.Currency.get(ctx.author.id)
+        if amount > c.coins:
+            await ctx.send(embed=utils.DefualtEmbed(desc=f"{receiver.mention} you don't have that many coins."))
             return
 
-        #! Always update after pay_exhange!
-        await utils.GemFunction.update_gems(user=ctx.author)
-        await utils.GemFunction.update_gems(user=receiver)
+        await utils.CoinFunctions.pay_user(payer=ctx.author, receiver=receiver, amount=amount)
 
-        async with self.bot.database() as db:
-            await c.save(db)
-            await c_r.save(db)
+        await ctx.send(embed=utils.DefualtEmbed(desc=f"**{ctx.author} sent {amount} coins to {receiver}!**"))
 
-        await msg.delete()
-        await ctx.send(embed=utils.SpecialEmbed(title=f"{ctx.author} Made a payment", desc=f"**{gems} {str(r.emoji)} was sent to {receiver}!**", guild=ctx.guild))
+        await self.server_log.send(embed=utils.LogEmbed(type="special", desc=f"{ctx.author} payed {amount} coins to {receiver}!"))
 
-        log = await utils.ChannelFunction.get_log_channel(guild=ctx.guild, log="currency")
-        await log.send(embed=utils.LogEmbed(type="positive", title=f"{ctx.author} made a payment.", desc=f"{ctx.author} payed {gems} {str(r.emoji)} was sent to {receiver}!", guild=ctx.guild))
+
 
 
 
