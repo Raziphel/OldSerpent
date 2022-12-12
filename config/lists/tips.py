0aeab45a4605d4267f4d13cb7d3de341c292624d
Raziphel <raziphel@gmail.com>
2022-12-12 10:54:15 -0600

    update tips.py

--- config/lists/tips.py
+++ config/lists/tips.py
@@ -8,10 +8,12 @@ Tips = ([
     "Do .profile to see your profile, duh!",
     "Tips are displayed here! Crazy right?",
     "Please don't lie about your age...",
-    "Chaos Channels only rules: No bullying, No TOS & Spoiler gross stuff. ",
     "Read channel topics!  If your confused...",
     "Razi needs to add more tips!",
     "Buy the XK Class role from the shop!!!  Has great channels.",
     "Daily's cap out after 365 days!",
     "Don't forget to claim your .daily!",
+    "Coins are only created from user activity.",
+    "The bot taxes everything, but also pays a lot of users.",
+    "Mention council if ya need assistant with a dip-shit.",
 ])
