de3fa7245b6fe06214a2644701205d122f3602c1
Basil <7519920+Baysul@users.noreply.github.com>
2022-12-22 16:36:28 -0800

    Profiles changes

--- cogs/users/profile.py
+++ cogs/users/profile.py
@@ -1,17 +1,31 @@
-#* Discord
+# * Discord
 from discord.ext.commands import command, Cog, BucketType, cooldown, group, ApplicationCommandMeta
-from discord import Member, Message, User, Game, User, ApplicationCommandOption, ApplicationCommandOptionType
-#* Additions
+from discord import Member, Message, User, Game, User, ApplicationCommandOption, ApplicationCommandOptionType, File
+# * Additions
 from datetime import datetime as dt, timedelta
 from asyncio import sleep
 from math import floor
-#* Utils
+# * Utils
 import utils
+
+import asyncio
+
+# Import the necessary modules
+from playwright.async_api import async_playwright
+
+profile_template = 'profile-template.html'
+
 
 class Profile(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        self.browser = None
+        self.bot.loop.create_task(self.bootstrap())
+
+    async def bootstrap(self):
+        playwright = await async_playwright().start()
+        self.browser = await playwright.chromium.launch(headless=True)
 
     @cooldown(1, 60, BucketType.channel)
     @command(
@@ -27,17 +41,72 @@ class Profile(Cog):
             ],
         ),
     )
-    async def profile(self, ctx, user:Member=None):
+    async def profile(self, ctx, user: Member = None):
         '''Shows a user's profile'''
         if not user:
             user = ctx.author
 
-        #! Quest 1 Complete
+        # ! Quest 1 Complete
         # await self.bot.get_cog('Quests').get_quest(user=user, quest_no=1, completed=True)
 
-        await self.base_profile(ctx=ctx, user=user, msg=None)
+        # await self.base_profile(ctx=ctx, user=user, msg=None)
+        file = await self.generate_screenshot(user)
 
+        await ctx.send(file=file)
 
+    async def generate_screenshot(self, member: Member):
+        def format_number(num):
+            if num < 1000:
+                return str(num)
+            elif num < 1000000:
+                return f"{num / 1000:.1f}K"
+            elif num < 1000000000:
+                return f"{num / 1000000:.1f}M"
+            else:
+                return f"{num / 1000000000:.1f}B"
+
+        moderation = utils.Moderation.get(member.id)
+        levels = utils.Levels.get(member.id)
+        currency = utils.Currency.get(member.id)
+        tracking = utils.Tracking.get(member.id)
+        staff_tracking = utils.Staff_Track.get(member.id)
+
+        if levels.level == 0:
+            required_exp = 10
+        elif levels.level < 5:
+            required_exp = levels.level * 25
+        else:
+            required_exp = round(levels.level ** 2.75)
+
+        current_experience = floor(levels.exp)
+        experience_percentage = floor(current_experience / required_exp) * 100
+
+        voice_days = floor((tracking.vc_mins / 60) / 24)
+
+        # Create a new page and set the HTML content of the page
+        page = await self.browser.new_page()
+
+        page.on("request", lambda request: print(">>", request.method, request.url))
+        page.on("response", lambda response: print("<<", response.status, response.url))
+
+        # Read HTML from the template
+        with open('profile-template.html', 'r') as html_template_hdl:
+            html_template = html_template_hdl.read()
+            html_template = html_template.format(
+                level=levels.level,
+                progress=experience_percentage,
+                experience=f'{current_experience:,}/{required_exp:,}',
+                coins=format_number(currency.coins),
+                messages=format_number(tracking.messages),
+                voice=format_number(voice_days)
+            )
+
+        await page.set_content(html_template)
+
+        screenshot_buffer = await page.locator('.outer-rectangle').screenshot()
+        file = File(screenshot_buffer, filename='profile.png')
+
+        return file
 
     async def base_profile(self, ctx, user, msg):
         if msg == None:
@@ -46,7 +115,7 @@ class Profile(Cog):
             await msg.edit(embed=utils.ProfileEmbed(type="Default", user=user))
 
         await msg.clear_reactions()
-        #! adds the reactions
+        # ! adds the reactions
         if ctx.channel.id in self.bot.config['fur-channels'].values():
             await msg.add_reaction("✨")
         if ctx.channel.id in self.bot.config['nsfw-fur-channels'].values():
@@ -73,11 +142,9 @@ class Profile(Cog):
             await self.base_profile(ctx=ctx, user=user, msg=msg)
             return
 
-
-
     @cooldown(1, 30, BucketType.user)
     @command(aliases=['Sona', 'fursona', 'Fursona'])
-    async def sona(self, ctx, user:Member=None):
+    async def sona(self, ctx, user: Member = None):
         '''Quick Post Sona'''
         if ctx.channel.id in self.bot.config['fur-channels'].values():
             await ctx.send("You can't post that nasty-ness here.", delete_after=10)
@@ -87,6 +154,7 @@ class Profile(Cog):
             user = ctx.author
         m = await ctx.send(embed=utils.ProfileEmbed(type="Sfw_Sona", user=user, quick=True))
 
+<<<<<<< Updated upstream
     @cooldown(1, 30, BucketType.user)
     @command(aliases=['NSona', 'nfursona', 'nFursona'])
     async def nsona(self, ctx, user:Member=None):
@@ -110,27 +178,44 @@ class Profile(Cog):
 
 
 
+=======
+>>>>>>> Stashed changes
     @cooldown(1, 5, BucketType.user)
     @command(aliases=['color', 'Color', 'Setcolor', 'SetColor'])
     async def setcolor(self, ctx, colour):
         '''Sets your user color'''
+<<<<<<< Updated upstream
         colour_value = utils.Colors.get(colour.lower()) 
         ss = utils.Settings.get(ctx.author.id)
+=======
+        colour_value = utils.Colors.get(colour.lower())
+        ss = utils.Tracking.get(ctx.author.id)
+>>>>>>> Stashed changes
 
         if colour_value == None:
             try:
                 colour_value = int(colour.strip('#'), 16)
             except ValueError:
+<<<<<<< Updated upstream
                 await ctx.send(embed=utils.SpecialEmbed(title="Incorrect colour usage!", guild=ctx.guild), delete_after=15)
+=======
+                await ctx.send(embed=utils.SpecialEmbed(title="Incorrect colour usage!", guild=ctx.guild),
+                               delete_after=5)
+>>>>>>> Stashed changes
                 return
 
         ss.color = colour_value
         async with self.bot.database() as db:
             await ss.save(db)
+<<<<<<< Updated upstream
         
         await ctx.send(embed=utils.DefualtEmbed(title="Your color setting has been set!", guild=ctx.guild, user=ctx.author), delete_after=15)
 
+=======
+>>>>>>> Stashed changes
 
+        await ctx.send(
+            embed=utils.DefualtEmbed(title="Your color setting has been set!", guild=ctx.guild, user=ctx.author))
 
 
 def setup(bot):
