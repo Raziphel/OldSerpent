cfa16555d5d35b504d53d81846d85cd99b346b99
Raziphel <raziphel@gmail.com>
2023-01-15 01:30:59 -0600

    making it pretty

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -162,7 +162,7 @@ class Loops(Cog):
 
         msg = await bot_usage.fetch_message(sti.message_id) #? msg
         embed=Embed(title=f"**[- Bot Usage Sticky -]**", 
-        description=f"**This channel is only for using bot commands!**\n\nthe Serpent bot has the `.` prefix for regular commands.\nThe Serpent's Music commands use the prefix `!` and both have a help command!", color=0xFF0000)
+        description=f"**This channel is only for using bot commands!**\nthe Serpent bot has the `.` prefix for regular commands.\nThe Serpent's Music commands use the prefix `!` and both have a help command!", color=0xFF0000)
         await msg.edit(content=f" ", embed=embed)
 
         #? Create the adult lounge sticky.
@@ -177,7 +177,7 @@ class Loops(Cog):
 
         msg = await lounge.fetch_message(sti.message_id) #? msg
         embed=Embed(title=f"**[- Adult Lounge Sticky -]**", 
-        description=f"**This channel is only for adults**\n\nBut NSFW content is not allowed!\nThe Auto Chat filters are off, but you can still be punished for beign overly offensive ofourse.", color=0xFF0000)
+        description=f"**This channel is only for adults**\n**But NSFW content is not allowed!**\nThe Auto Chat filters are off, but you can still be punished\n for being overly offensive ofourse.", color=0xFF0000)
         await msg.edit(content=f" ", embed=embed)
 
 
