0219d467cfe8fbb42af14aed327f3aee6b6d4a2b
Raziphel <raziphel@gmail.com>
2023-01-22 01:49:33 -0600

    AAAAAAAAAAAAAAA

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -167,8 +167,8 @@ class Loops(Cog):
             if last_message.id == sti.message_id:
                 continue
             else:
-                    msg = await channel.fetch_message(sti.message_id) #? get last message
-                    await msg.delete()
+                msg = await channel.fetch_message(sti.message_id) #? get last message
+                await msg.delete()
                 msg = await channel.send('**Loading sticky message**')
                 sti.message_id = msg.id
 
