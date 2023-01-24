981b07fa9621238d19443d9c75a4a04c87e9cb9c
Basil <7519920+Baysul@users.noreply.github.com>
2023-01-23 18:48:13 -0800

    Converted pay command to interaction command

--- cogs/users/payment.py
+++ cogs/users/payment.py
@@ -1,46 +1,81 @@
 # Discord
-from discord.ext.commands import command, cooldown, BucketType, Cog
-from discord import Member, Message, User
+from discord import User, ApplicationCommandOption, ApplicationCommandOptionType
+from discord.ext.commands import command, cooldown, BucketType, Cog, ApplicationCommandMeta
+
 # Utils
 import utils
+
 
 class Payment(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @property  #! The members logs
+    @property  # ! The members logs
     def discord_log(self):
-        return self.bot.get_channel(self.bot.config['channels']['server']) #?Coins log channel
+        return self.bot.get_channel(self.bot.config['channels']['server'])  # ?Coins log channel
 
     @cooldown(1, 30, BucketType.user)
-    @command(aliases=['send'])
-    async def pay(self, ctx, receiver:User=None, amount:int=0):
-        '''sending payemnts to other members'''
+    @command(
+        aliases=['send'],
+        application_command_meta=ApplicationCommandMeta(
+            options=[
+                ApplicationCommandOption(
+                    name="recipient",
+                    description="The user you want to send coins to.",
+                    type=ApplicationCommandOptionType.user,
+                    required=True,
+                ),
+                ApplicationCommandOption(
+                    name="amount",
+                    description="The amount of coins you'd like to send.",
+                    type=ApplicationCommandOptionType.integer,
+                    required=True,
+                ),
+            ],
+        ),
+    )
+    async def pay(self, ctx, recipient: User = None, amount: int = 0):
+        """Send a payment to another member."""
         coin_e = self.bot.config['emotes']['coin']
-        if not receiver:
-            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} `->pay (user) (amount)`"))
-            return
-        if receiver == ctx.author:
-            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} You can't pay yourself coins! stupid..."))
-            return
+
+        # Check if the recipient is the same as the user.
+        if recipient == ctx.author:
+            return ctx.interaction.response.send_message(
+                embed=utils.DefualtEmbed(
+                    description=f"{ctx.author} You can't pay yourself coins! stupid..."
+                )
+            )
+
         if amount <= 10:
-            await ctx.send(embed=utils.DefualtEmbed(desc=f"{ctx.author} Has to be more than 10!"))
-            return
+            return ctx.interaction.response.send_message(
+                embed=utils.DefualtEmbed(
+                    description=f"{ctx.author} Has to be more than 10!"
+                )
+            )
+
+        # Check if the user has enough coins.
         c = utils.Currency.get(ctx.author.id)
         if amount > c.coins:
-            await ctx.send(embed=utils.DefualtEmbed(desc=f"{receiver.mention} you don't have that many coins."))
-            return
+            return ctx.interaction.response.send_message(
+                embed=utils.DefualtEmbed(
+                    description=f"{recipient.mention} you don't have that many coins."
+                )
+            )
 
-        await utils.CoinFunctions.pay_user(payer=ctx.author, receiver=receiver, amount=amount)
+        await utils.CoinFunctions.pay_user(payer=ctx.author, receiver=recipient, amount=amount)
 
-        await ctx.send(embed=utils.DefualtEmbed(desc=f"**{ctx.author} sent {amount:,}x {coin_e} to {receiver}!**"))
+        ctx.interaction.response.send_message(
+            embed=utils.DefualtEmbed(
+                description=f"**{ctx.author} sent {amount:,}x {coin_e} to {recipient}!**"
+            )
+        )
 
-        await self.discord_log.send(embed=utils.LogEmbed(type="special", desc=f"{ctx.author} payed {amount} coins to {receiver}!"))
-
-
-
-
-
+        await self.discord_log.send(
+            embed=utils.LogEmbed(
+                type="special",
+                desc=f"{ctx.author} payed {amount} coins to {recipient}!"
+            )
+        )
 
 
 def setup(bot):
