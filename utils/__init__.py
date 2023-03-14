29bc9cdffbda45eae6058d1e18125c7206c86b6c
Raziphel <raziphel@gmail.com>
2023-03-13 22:57:15 -0500

    fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -34,6 +34,7 @@ from utils.sql.admin.sticky import Sticky
 #? Functions
 from utils.functions.userfunction import UserFunction
 from utils.functions.coinfunctions import CoinFunctions
+from utils.functions.TimeConverter import TimeConverter
 
 
 #? Additions
