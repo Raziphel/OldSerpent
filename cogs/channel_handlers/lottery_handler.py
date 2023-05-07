2b0ae3c7f0000dd2f0c08ff091daa64153583ca6
Raziphel <raziphel@gmail.com>
2023-05-06 21:44:07 -0500

    WEE

--- cogs/channel_handlers/lottery_handler.py
+++ cogs/channel_handlers/lottery_handler.py
@@ -67,7 +67,7 @@ class lottery_handler(Cog):
                 lot.lot_time = dt.utcnow()
 
             tf = lot.lot_time + timedelta(hours=72)
-            t = dt(1, 1, 1) - (tf - dt.now())
+            t = dt(1, 1, 1) + (tf - dt.now())
 
             sorted_rank = utils.Items.sort_tickets()
             ranks = sorted_rank[:20]
@@ -76,7 +76,7 @@ class lottery_handler(Cog):
                 user = self.bot.get_user(i.user_id)
                 if user != None:
                     users.append(user)
-            text = [f"The Weekly Lottery Has: **{t.day} days, {t.hour} hours and {t.minute} minutes** remaining!\n\n**Top Ticket Holders:**\n"]
+            text = [f"The Weekly Lottery Has: **{t.day-1} days, {t.hour} hours and {t.minute} minutes** remaining!\n\n**Top Ticket Holders:**\n"]
             for index, (user, rank) in enumerate(zip(users, ranks)):
                 if index < 20:
                     text.append(f"#{index+1} **{user}** --> {floor(rank.lot_tickets):,} 🎟")
