f64f9bc73a0e181220193571194da1a5886b5085
Raziphel <raziphel@gmail.com>
2023-07-28 14:14:08 -0500

    new bot statuses

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -78,7 +78,7 @@ class Loops(Cog):
                 except: pass
 
         #* Setting the bot status.
-        playing = choice(["Convincing Eve to eat an apple!", "Slitherin' in the Garden...", "Commiting Atrocities.", "Starting Plagues", "Worshipin' The Bearer of light", "Devilish intentions!"])
+        playing = choice(["Trans-Rights are Human Rights!", "It's okay to be gay!", "Hate has no place here.", "Chill & Nice is the new cool.", "Why be mad? Be glad."])
         await self.bot.change_presence(activity=Game(name=playing))
 
         #* Setting the Channel Stats.
