7cf9418ded01cb0946141a6dbebddc1cb3d74b29
Raziphel <raziphel@gmail.com>
2023-08-07 15:32:41 -0500

    ugh

--- cogs/constants/listeners.py
+++ cogs/constants/listeners.py
@@ -29,19 +29,19 @@ class Listeners(Cog):
 
 
 
-    @Cog.listener('on_message')
-    async def music_handler_listener(self, message):
-        '''Looks for spotify music!'''
+    # @Cog.listener('on_message')
+    # async def music_handler_listener(self, message):
+    #     '''Looks for spotify music!'''
 
-        guild = self.bot.get_guild(self.bot.config['garden_id']) #? Guild
-        ch = guild.get_channel(1138195661288914995) #? music Channel
+    #     guild = self.bot.get_guild(self.bot.config['garden_id']) #? Guild
+    #     ch = guild.get_channel(1138195661288914995) #? music Channel
 
-        #? Check for music channel
-        if message.channel.id == ch.id:
-            return
+    #     #? Check for music channel
+    #     if message.channel.id == ch.id:
+    #         return
 
-        if search(self.spotifyReg, message.content):
-            await ch.send(f"{message.author} Posted: {message.content} in {message.channel}")
+    #     if search(self.spotifyReg, message.content):
+    #         await ch.send(f"{message.author} Posted: {message.content} in {message.channel}")
 
 
     @Cog.listener('on_message')
