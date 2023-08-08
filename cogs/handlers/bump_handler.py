6bad0a981593a91e73a8525c24353f92be76ed0f
Raziphel <raziphel@gmail.com>
2023-08-08 12:33:56 -0500

    Woohoo

--- cogs/handlers/bump_handler.py
+++ cogs/handlers/bump_handler.py
@@ -4,11 +4,13 @@ import discord
 from discord.ext import commands
 
 
-BUMP_TEXT = """
-<@&1130827884324458607> Please run `/bump` with the Disboard bot.
-Along with bumping the server on https://Discord.me/Dashboard  
+DISCORDME_BUMP_TEXT = """
+<@&1130827884324458607> Please bump the server on https://Discord.me/Dashboard  
 """
 
+DISBOARD_BUMP_TEXT = """
+<@&1130827884324458607> Please run `/bump` with the Disboard bot.
+"""
 
 class BumpHandler(commands.Cog):
 
@@ -18,10 +20,19 @@ class BumpHandler(commands.Cog):
     DISCORDME_TIMEOUT = timedelta(hours=6)  # TODO move to config
     DISCORDME_GRACE_PERIOD = timedelta(minutes=5)  # TODO move to config
 
+
+
+    DISBOARD_USER_ID = 1020831331736887406  # TODO move to config
+
+    DISBOARD_TIMEOUT = timedelta(hours=2)  # TODO move to config
+    DISBOARD_GRACE_PERIOD = timedelta(minutes=5)  # TODO move to config
+
+
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
         self.notify_task = None
+        self.notify_task2 = None
 
 
     async def bump_timer(self):
@@ -35,6 +46,7 @@ class BumpHandler(commands.Cog):
         )
         channel = self.bot.get_partial_messageable(self.BUMP_CHANNEL_ID)
         await channel.send(BUMP_TEXT)
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -62,6 +74,58 @@ class BumpHandler(commands.Cog):
         if self.notify_task is not None:
             self.notify_task.cancel()
         self.notify_task = asyncio.create_task(self.bump_timer())
+
+
+
+
+
+    async def bump_timer2(self):
+        """
+        Wait for N minutes, send a message in the specified channel.
+        """
+
+        await asyncio.sleep(
+            (self.DISBOARD_TIMEOUT + self.DISBOARD_GRACE_PERIOD)
+            .total_seconds()
+        )
+        channel = self.bot.get_partial_messageable(self.BUMP_CHANNEL_ID)
+        await channel.send(BUMP_TEXT)
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if self.notify_task2 is not None:
+            self.notify_task2 = asyncio.create_task(self.bump_timer2())
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        """
+        Listens for the "this guild has been bumped" message, and
+        queues up a reminder to go in 2 hours.
+        """
+
+        # Make sure we're in the right place
+        if message.guild is None:
+            return
+
+        # Check the message sent
+        if not message.embeds:
+            return
+        embed = message.embeds[0]
+        if not isinstance(embed.description, str):
+            return
+        if "bump done!" not in embed.description.casefold():
+            return
+
+
+        # Alright, reset/start timer
+        if self.notify_task2 is not None:
+            self.notify_task2.cancel()
+        self.notify_task2 = asyncio.create_task(self.bump_timer2())
+
+
+
+
 
 
 def setup(bot: commands.Bot):
