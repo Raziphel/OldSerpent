360a1b43740d1e8517e91bd4d813619ca6907950
Raziphel <raziphel@gmail.com>
2023-08-25 21:15:14 -0500

    oops

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -321,7 +321,7 @@ class Loops(Cog):
                     embed=Embed(description=f"```fix\n█ Issues Sticky █\n```\n**This channel is for pinging staff about issues happening the SCP servers!**\n*Please follow these guidelines before you ping!*\n\n**<@&891793700932431942>** - Ping for Major bugs or anything if you think its important enough.\n**<@&1068389027222405221>** - Ping for anything SCP Server related.\n**<@&1068389119195107378>** - Ping for anything Discord related.", color=randint(1, 0xffffff))
                 if channel in suggestion_channels:
                     embed=Embed(description=f"```fix\n█ Suggestion Sticky █\n```\n**Please do not just hold conversations or make replies here!**\n\nYou can create a thread to reply to suggestions!", color=randint(1, 0xffffff))
-                if channel in adult_chat:
+                if channel == adult_chat:
                     embed=Embed(description=f"```fix\n█ NEW Adult Chat █\n```\n**Please do not post Pornography!**\n\nIts for adult topics, but no pornography should be shared!", color=randint(1, 0xffffff))
                 try:
                     await msg.edit(content=f" ", embed=embed)
