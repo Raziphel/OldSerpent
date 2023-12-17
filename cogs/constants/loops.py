921f801ac249a5c933104a4fb2a6a17666396986
Raziphel <raziphel@gmail.com>
2023-12-17 01:25:05 -0600

    wee

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -78,7 +78,7 @@ class Loops(Cog):
                 except: pass
 
         #* Setting the bot status.
-        playing = choice(["Trans-Rights are Human Rights!", "If you don't fites 4 trans rites you'll get bites."])
+        playing = choice(["Trans-Rights are Human Rights!", "If you don't fites 4 trans rites you'll get bites.", "on the SCP Servers!", "Playing on the Lethal Company Modpack!", "Playing on the Minecraft Server!"])
         await self.bot.change_presence(activity=Game(name=playing))
 
         #* Setting the Channel Stats.
