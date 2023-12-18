07b7dd693a809db6de33be16aa4dac2f3821637a
Raziphel <raziphel@gmail.com>
2023-12-18 09:15:06 -0600

    a

--- cogs/constants/loops.py
+++ cogs/constants/loops.py
@@ -216,14 +216,14 @@ class Loops(Cog):
                 supps += 1
 
 
-        embed1=Embed(title=f"**[- Discord Statistics! -]**", 
-        description=f"**This show's stats about the Discord Server!**\n\n🎭 Members: **{members:,}**\n💕 Supporters: **{supps:,}**\n<:Ascended:1095161421853098108> Ascended: **{t3s}**\n<:Acolyte:1095161419357499503> Acolyte: **{t2s}**\n<:Initiate:1095161420297011200> Initiate: **{t1s}**\n<:Nitro:1095491689029849189>  Boosters: **{nitros}**", color=0xFF0000)
+        embed1=Embed(title=f"**[- Supporter Statistics! -]**", 
+        description=f"**This show's stats about server support!**\n\n💕 Supporters: **{supps:,}**\n<:Ascended:1095161421853098108> Ascended: **{t3s}**\n<:Acolyte:1095161419357499503> Acolyte: **{t2s}**\n<:Initiate:1095161420297011200> Initiate: **{t1s}**\n<:Nitro:1095491689029849189>  Boosters: **{nitros}**", color=0xFF0000)
 
         embed2=Embed(title=f"**[- Economy Statistics! -]**", 
         description=f"**This show's all the aspects of the Serpent's Economy!**\n\n{coin_e} Total: **{floor(total_coins):,}** Coins\n🐍 Serpent's: **{floor(sc.coins):,}** Coins\n🎟 Current Tickets: **{floor(total_tix):,}**", color=0x00FF00)
 
         embed3=Embed(title=f"**[- Garden Statistics! -]**", 
-        description=f"📚 Channels: **{total_channels:,}**\n 🎭 Roles: **{total_roles:,}**", color=0x0000FF)
+        description=f"**This show's stats about the Discord Server!**\n\n👥 Members: **{members:,}**\n📚 Channels: **{total_channels:,}**\n 🎭 Roles: **{total_roles:,}**", color=0x0000FF)
 
 
         await msg1.edit(content=f" ", embed=embed1)
